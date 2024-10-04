bdaddabb6103ad741ff8caa62c85811ddcbaea0e
taewoo.lee <gunmong83@gmail.com>
2024-10-04 17:02:16 +0900

    버튼 생성 애니메이션 등록

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="app">
     <div class="background1">
-      <div
-        class="line"
-        v-for="(line, index) in lines"
-        :key="index"
-        :ref="'line-' + index"
-        :style="getLineStyle(index)"
-      ></div>
+      <div class="line" v-for="(line, index) in lines" :key="index" :ref="'line-' + index" :style="getLineStyle(index)">
+      </div>
+      <div class="intersection" v-for="(line, index) in lines" :key="index" :style="getInterAreaStyle(index)"
+        :ref="'text-' + index" @mouseenter="handleMouseEnter(index)" @mouseleave="handleMouseLeave(index)">
+      </div>
     </div>
   </div>
 </template>
@@ -21,19 +19,24 @@ export default {
   data() {
     return {
       lines: new Array(12).fill(null), // 12개의 라인 생성
+      isMouseOver: [], // 각 라인의 애니메이션 타임라인 저장
+      lineCoord: [],
     };
+  },
+  created() {
+    this.calcLineCoord();
   },
   mounted() {
     this.animateLines();
   },
   methods: {
     animateLines() {
-      const timeline = gsap.timeline({repeatDelay: 0.3});
+      const timeline = gsap.timeline({ repeatDelay: 0.3 });
       const rows = 3;
       const cols = 4;
       for (let row = 0; row < rows; row++) {
         const lineIndices = Array.from({ length: cols }, (_, col) => row * cols + col);
-        
+
         if (row % 2 === 1) {
           // 짝수 행일 경우 순서를 반대로
           lineIndices.reverse();
@@ -43,35 +46,129 @@ export default {
           const lineRef = this.$refs[`line-${index}`][0];
           timeline.to(lineRef, {
             rotation: "-=90", // 90도씩 추가로 회전
-            duration: 0.7, // 각 라인의 회전 애니메이션 지속 시간
+            duration: 0.5, // 각 라인의 회전 애니메이션 지속 시간
             ease: "power1.inOut", // 부드러운 애니메이션 효과
-          }, i===0? '+=0.2' : '-=0.5'); // 첫 번째 라인은 텀을 두고 시작, 나머지는 겹쳐서 진행
+          }, i === 0 ? '-=0.0' : '-=0.4');
         });
       }
 
       // 애니메이션이 끝나면 다시 시작하도록 설정
       timeline.eventCallback("onComplete", () => {
-        this.animateLines();
+        setTimeout(() => {
+          this.animateLines();
+        }, 2000); // 2초 대기
       });
     },
-    getLineStyle(index) {
+    handleMouseEnter(index) {
+      // 현재 라인의 애니메이션 멈춤
+      const lineRef = this.$refs[`line-${index}`][0];
+      const textRef = this.$refs[`text-${index}`][0];
+      this.isMouseOver[index] = true;
+      const timeline = gsap.timeline();
+
+      timeline.to(lineRef, {
+        x: `-5vw`,
+        duration: 0.5,
+        width: '10vw',
+        color: 'transparent',
+        backgroundColor: 'transparent',
+      });
+      
+      timeline.call(() => {
+        if (!this.isMouseOver[index]) {
+          timeline.pause();
+        }
+      })
+
+      timeline.to(textRef, {
+        duration: 0.5,
+        backgroundColor: 'transparent',
+        textAlign: 'center',
+        fontSize: '2vw',
+        color: '#ffffff',
+        innerHTML: 'PORTFOLIO',
+        ease: 'power1.inOut',
+      });
+
+      lineRef.style.cursor = 'pointer'; // 버튼처럼 변경
+    
+    },
+
+    handleMouseLeave(index) {
+      const lineRef = this.$refs[`line-${index}`][0];
+      const textRef = this.$refs[`text-${index}`][0];
+      this.isMouseOver[index] = false;
+      const timeline = gsap.timeline();
+      
+      timeline.to(textRef, {
+        duration: 0.5,
+        fontSize: '0vw',
+        ease: 'power1.inOut',
+      });
+      timeline.call(() => {
+        if (this.isMouseOver[index]) {
+          timeline.pause();
+        }
+      })
+      timeline.to(lineRef, {
+        x: "0vw",
+        duration: 0.5,
+        width: '0.15vw',
+        color: '#ffffff',
+        backgroundColor: '#ffffff',
+      });
+
+      lineRef.style.cursor = 'default';
+
+    },
+    calcLineCoord() {
       const rows = 3;
       const cols = 4;
-      const row = Math.floor(index / cols);
-      const col = index % cols;
       const spacingX = 20.5; // X 축 간격 설정 (vw 단위)
-      const spacingY = 19.5; // Y 축 간격 설정 (vh 단위)
-      const degree = (row % 2) ? "135deg" : "45deg";
+      const spacingY = 19.5; // Y 축 간격 설정 (vw 단위)
+      for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+          const top = 5 + row * spacingY;
+          const left = 19.5 + col * spacingX;
+          const degree = (row % 2) ? "135deg" : "45deg";
+          this.lineCoord.push({ top, left, degree });
+        }
+      }
+    },
+    getLineStyle(index) {
+      const { top, left, degree } = this.lineCoord[index];
       return {
         width: '0.15vw',
         height: '17vw',
         backgroundColor: '#ffffff',
         position: 'absolute',
-        top: `${5 + row * spacingY}vw`,
-        left: `${19.5 + col * spacingX}vw`,
+        top : `${top}vw`,
+        left : `${left}vw`,
         transform: `rotate(${degree})`, // 초기 45도 회전
       };
     },
+    getInterAreaStyle(index) {
+      const rows = 3;
+      const cols = 4;
+      const size = 10;
+      const row = Math.floor(index / cols);
+      const col = index % cols;
+      const spacingX = 20.5; // X 축 간격 설정 (vw 단위)
+      const spacingY = 19.5; // Y 축 간격 설정 (vh 단위)
+      return {
+        width: `${size}vw`,
+        height: `${size}vw`,
+        backgroundColor: '#ffffff',
+        position: 'absolute',
+        top: `${8.5 + row * spacingY}vw`,
+        left: `${19.5 + col * spacingX - size / 2}vw`,
+        backgroundColor: 'transparent',
+        border: '0px solid #ffffff',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      };
+    }
   },
 };
 </script>
@@ -91,11 +188,5 @@ export default {
   background-size: contain;
   background-repeat: no-repeat;
   overflow-y: scroll;
-}
-
-.line {
-  width: 1px;
-  height: 15vw;
-  background-color: #ffffff;
 }
 </style>
