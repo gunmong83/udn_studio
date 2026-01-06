869b74f400e1c2b84109a97e9dee39ed7f5628b1
taewoo.lee <gunmong83@gmail.com>
2026-01-06 20:13:17 +0900

    modify outline of portfolio and add vuetify

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import './style.css'
 import Home from './Home.vue'
 
-createApp(Home).mount('#app')
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+createApp(Home).use(vuetify).mount('#app')
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "vuetify/styles" as *;',
+      },
+    },
+  },
   server: {
     port: 8080
-  }
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
